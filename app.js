3f2cd6d16528468321075ecabb6ca3c925e345ef
andrewv <andy@fastmail.com.au>
2016-05-24 13:55:33 +1000

    Get JS working

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+ window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '{your-app-id}',
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    
+      // Get Embedded Video Player API Instance
+      var my_video_player;
+      FB.Event.subscribe('xfbml.ready', function(msg) {
+        if (msg.type === 'video') {
+          my_video_player = msg.instance;
+        }
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
 $('#PlayButton').on('click', function (e) {
 
      my_video_player.play();
